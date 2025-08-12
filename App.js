786307bc9142612d941db4decc456b8ac505038b
Graziani V Zafolin <zanfolin@gmail.com>
2025-08-12 08:37:23 -0300

    [fix] App.js - Corrigi o texto do título

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Projeto TCC</Text>
+      <Text>Projeto TCC - Turma B</Text>
       <StatusBar style="auto" />
     </View>
   );
