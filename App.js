c947fdca8efe754688a033035ff1c876f685eefc
Graziani V Zafolin <zanfolin@gmail.com>
2025-09-08 10:00:08 -0300

    [feat] Get Users

--- App.js
+++ App.js
@@ -1,10 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import {
+  ActivityIndicator,
+  Button,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 export default function App() {
+  //Processo do GET - Solicitar dados do servidor
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchData = async () => {
+    const API = "http://192.168.100.109:3000/api";
+    const URL = `${API}/user`;
+    console.log(URL);
+    setLoading(true);
+
+    try {
+      const response = await fetch(URL);
+      const json = await response.json();
+      console.log(json);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text>Projeto TCC - Turma B</Text>
+      <View>
+        {loading ? (
+          <ActivityIndicator />
+        ) : (
+          <Button title="Buscar Dados" onPress={fetchData} />
+        )}
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +48,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
